018cc0d8e5fe30c23bbf1e66dd8821f46728f124
Davide <ghiotto.davidenko@gmail.com>
2019-07-25 11:33:57 +0200

    small fix

--- src/io/io.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const io = function() {};
-io.prototype = {
-  read: function(filename, format) {},
-  write: function(filename, data) {},
-  append: function(filename, data) {}
-};
-module.exports = io;
